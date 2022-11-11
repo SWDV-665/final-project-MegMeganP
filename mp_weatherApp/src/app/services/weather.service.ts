c510c531c6d4bf920c3224839e479c1e84e1b338
Megan Perry <80060671+MegMeganP@users.noreply.github.com>
2022-11-10 19:47:13 -0700

    hid sensitive information-API key, etc

--- mp_weatherApp/src/app/services/weather.service.ts
+++ mp_weatherApp/src/app/services/weather.service.ts
@@ -12,9 +12,9 @@ export class WeatherService {
   ) { }
 
   getWeatherData(): Observable<any> {
-    let lat = 33.3062;
-    let lon = -111.8413;
-    let apiId = '9993609861517d431c5805fac62093cb';
+    let lat = 000000;
+    let lon = 00000;
+    let apiId = 'xxxxxxxxxx';
     let queryString = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly,alerts&appid=${apiId}`;
 
     return this.http.get(queryString);
